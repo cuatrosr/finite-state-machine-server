4bc8636f78077dd6d67a7c23e57f34e895d4465d
Santiago Arevalo Valencia <santiguitar2003@gmail.com>
2022-09-28 23:31:31 -0500

    Documented model classes and methods

--- src/main/java/co/edu/icesi/machine/model/Mealy.java
+++ src/main/java/co/edu/icesi/machine/model/Mealy.java
@@ -15,9 +15,20 @@ public class Mealy {
     private List<String> stimulus;
     private List<MealyStates> states;
 
+    /**
+     * Returns the list of states of a Mealy machine that are inaccessible
+     * @param from state from which the search for inaccessible states is being started
+     * @return the list of inaccessible states
+     */
+
     public List<MealyState> searchRelationsStates(String from) {
         return Objects.requireNonNull(states.stream().filter(e -> e.getRoot().equals(from)).findFirst().orElse(null)).getStates();
     }
+
+    /**
+     * Delete the inaccessible states from Mealy machine
+     * @param state inaccessible state to remove
+     */
 
     public void removeRelationsState(String state) {
         states.remove(states.stream().filter(e -> e.getRoot().equals(state)).findFirst().orElse(null));
--- src/main/java/co/edu/icesi/machine/model/Moore.java
+++ src/main/java/co/edu/icesi/machine/model/Moore.java
@@ -15,9 +15,20 @@ public class Moore {
     private List<String> stimulus;
     private List<MooreState> states;
 
+    /**
+     * Returns the list of states of a Moore machine that are inaccessible
+     * @param from state from which the search for inaccessible states is being started
+     * @return the list of inaccessible states
+     */
+
     public List<String> searchRelationsStates(String from) {
         return Objects.requireNonNull(states.stream().filter(e -> e.getRoot().equals(from)).findFirst().orElse(null)).getStates();
     }
+
+    /**
+     * Delete the inaccessible states from Moore machine
+     * @param state inaccessible state to remove
+     */
 
     public void removeRelationsState(String state) {
         states.remove(states.stream().filter(e -> e.getRoot().equals(state)).findFirst().orElse(null));
