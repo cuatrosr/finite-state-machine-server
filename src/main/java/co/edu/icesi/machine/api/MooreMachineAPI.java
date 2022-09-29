5de8d17b6b4cb879afc736d645a9a7d5b8ef905d
David Montaño <cuatro04sr@gmail.com>
2022-09-28 22:57:57 -0500

    fix: solved cors cross origin problem

--- src/main/java/co/edu/icesi/machine/api/MooreMachineAPI.java
+++ src/main/java/co/edu/icesi/machine/api/MooreMachineAPI.java
@@ -1,10 +1,12 @@
 package co.edu.icesi.machine.api;
 
 import co.edu.icesi.machine.dto.MooreDTO;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@CrossOrigin(origins = "*")
 @RequestMapping("/machine/moore")
 public interface MooreMachineAPI {
 
